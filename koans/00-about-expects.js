d9d243e223f094a4ed0d3a220a1932c8b5ecae37
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-03-30 18:08:32 -0700

    feat: wip additional intro assertions

--- koans/00-about-expects.js
+++ koans/00-about-expects.js
@@ -1,22 +1,36 @@
 describe('About Expects', function () {
-	// We shall contemplate truth by testing reality, via spec expectations.
-	it('should expect true', function () {
-		// Your journey begins here: Replace the word false with true
-		expect(false).toBeTruthy();
+	/**
+	 * In testing, an expectation or expect is a piece of code
+	 * that asserts how a program should behave. J
+	 * The `expect` function here is called with a value, and from there
+	 * we chain methods that explain what we want that value to be.
+	 *
+	 * For example: can use the methods `.toBeTruthy()` and `.toBeFalsy()` to test if something is, well, truthy or falsy.
+	 */
+	it('should expect predictable truthiness and falsiness for boolean values', function () {
+		expect(true).toBeTruthy();
+		expect(false).toBeFalsy();
+
+		/**
+		 * This test is failing.
+		 */
+		 it('should expect predictable truthiness and falsiness for boolean values', function() {
+
+		 });
 	});
 
 	// To understand reality, we must compare our expectations against reality.
 	it('should expect equality', function () {
-		var expectedValue = FILL_ME_IN;
+		var expectedValue = 2;
 		var actualValue = 1 + 1;
 
 		expect(actualValue === expectedValue).toBeTruthy();
 	});
 
-	// Some ways of asserting equality are better than others.
+	// Some ways of asserting equality are better than others!
 	it('should assert equality a better way', function () {
-		var expectedValue = FILL_ME_IN;
-		var actualValue = 1 + 1;
+		var expectedValue = [2];
+		var actualValue = [2];
 
 		// toEqual() compares using common sense equality.
 		expect(actualValue).toEqual(expectedValue);
@@ -24,15 +38,36 @@ describe('About Expects', function () {
 
 	// Sometimes you need to be precise about what you "type."
 	it('should assert equality with ===', function () {
-		var expectedValue = FILL_ME_IN;
+		var expectedValue = '2';
 		var actualValue = (1 + 1).toString();
 
 		// toBe() will always use === to compare.
 		expect(actualValue).toBe(expectedValue);
 	});
+});
 
-	// Sometimes we will ask you to fill in the values.
-	it('should have filled in values', function () {
-		expect(1 + 1).toEqual(FILL_ME_IN);
+describe('About JavaScript', function () {
+	it('considers numbers to be equal to their string representation', () => {
+		expect(1 == '1').toBeTruthy();
+		expect(1 != '1').toBeFalsy();
+	});
+
+	it('knows that numbers and strings are not exactly the same', () => {
+		expect(1 === '1').toBeFalsy();
+		expect(1 !== '1').toBeTruthy();
+	});
+
+	it('joins parts as string when using the plus operator', () => {
+		expect(1 + 'a').toEqual('1a');
+	});
+
+	it('operates integers before joining the string', () => {
+		expect(1 + 1 + '2').toEqual('22');
+	});
+
+	it('knows the type of the variable', () => {
+		const x = 1;
+
+		expect(typeof x).toEqual(FILL_ME_IN);
 	});
 });
